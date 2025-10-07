0a885d56401e067745aedfb2ab6edd9582014369
Federico Moretti <fmoretti@vivocha.com>
2025-10-07 15:48:16 +0200

    fix: store 'getMinDateTime' in a variable 'minDateTime'

--- src/app/interaction-theme/data-collection/form/form.component.ts
+++ src/app/interaction-theme/data-collection/form/form.component.ts
@@ -18,6 +18,7 @@ export class FormComponent implements OnInit {
   fields = [];
   form: FormGroup;
   hasRequired = false;
+  minDateTime = this.getMinDateTime();
   @Input() dc;
   @Input() country;
   @Input() readMode = false;
